2ae1e63ca5b424303b528cd6bd0c3b73dfbe937b
engineer-lanya <engineerlanya@gmail.com>
2026-05-24 16:09:18 +0300

    Update llll.py

--- llll.py
+++ llll.py
@@ -165,13 +165,12 @@ def dept_admin_panel():
             with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
                 df_report.to_excel(writer, index=False, sheet_name='General_Report')
             
-           csv_data = df.to_csv(index=False).encode('utf-8-sig')
-
-           st.download_button(
-            label="📥داگرتنی ڕاپۆرت",
-            data=csv_data,
-            file_name="attendance_report.csv",
-            mime="text/csv" 
+            csv_data = df.to_csv(index=False).encode('utf-8-sig')
+            st.download_button(
+                 label="📥داگرتنی ڕاپۆرت",
+                 data=csv_data,
+                 file_name="attendance_report.csv",
+                 mime="text/csv" 
             )
         else:
             st.warning("هیچ زانیارییەک بۆ ئەم قۆناغە نییە")
